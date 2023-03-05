b833e479e8162d5ecc5cc83a174fdca675614fa9
Chibu O <56417244+ChibuO@users.noreply.github.com>
2023-03-05 03:46:16 -0600

    Added other harris functions

--- scan_largearray_kernel.cu
+++ scan_largearray_kernel.cu
@@ -9,7 +9,8 @@
 #define LOG_NUM_BANKS 5
 // Lab4: You can use any other block size you wish.
 #define BLOCK_SIZE 256
-
+#define CONFLICT_FREE_OFFSET(n) \
+  ((n) >> NUM_BANKS + (n) >> (2 * LOG_NUM_BANKS)) 
 // Lab4: Host Helper Functions (allocate your own data structure...)
 
 
@@ -17,7 +18,145 @@
 
 
 // Lab4: Kernel Functions
-__global__ void scanKernel(float *g_odata, float *g_idata, int n) {
+__global__ void naiveScan(float *g_odata, float *g_idata, int n);
+__global__ void sweepScan(float *g_odata, float *g_idata, int n);
+
+
+__global__ void simp(float *g_odata, float *g_idata, int n) {
+  extern __shared__ float temp[]; // allocated on invocation
+  
+  int tid = threadIdx.x;
+  int offset = 1;
+  
+  temp[2*tid] = g_idata[2*tid]; //load 1st and second element into shared mem
+  temp[2*tid+1] = g_idata[2*tid+1];
+  
+  /*
+  loading two elements from separate halves of the array, we avoid these bank conflicts.
+  Also, to avoid bank conflicts during the tree traversal, we need to 
+  add padding to the shared memory array every NUM_BANKS (16) elements
+  */
+  int ai = tid;
+  int bi = tid + (n/2);
+  
+  int bankOffsetA = CONFLICT_FREE_OFFSET(ai);
+  int bankOffsetB = CONFLICT_FREE_OFFSET(bi);
+  
+  temp[ai + bankOffsetA] = g_idata[ai];
+  temp[bi + bankOffsetB] = g_idata[bi];
+  
+  for (int d = n >> 1; d > 0; d >>= 1) {  // build sum in place up the tree
+    //divide n by 2 each time, probably shift so no decimals
+    __syncthreads();
+    
+    if (tid < d) {
+      int ai = offset * (2 * tid + 1) - 1;
+      int bi = offset * (2 * tid + 2) - 1;
+      
+      temp[bi] += temp[ai];
+    }
+    
+    offset *= 2;
+  }
+  
+  if (tid == 0) {
+    temp[n - 1 + CONFLICT_FREE_OFFSET(n - 1)] = 0; // clear the last element
+  }
+  
+  for (int d = 1; d < n; d *= 2)  {//traverse down tree & build scan
+    offset >>= 1;
+    __syncthreads();
+    
+    if (tid < d) {
+      int ai = offset*(2*tid+1)-1; 
+      int bi = offset*(2*tid+2)-1; 
+      
+      float t = temp[ai];
+      temp[ai] = temp[bi]; 
+      temp[bi] += t;
+    }
+  }
+  
+  __syncthreads();
+  
+  //write results to array
+  g_odata[ai] = temp[ai + bankOffsetA]; 
+  g_odata[bi] = temp[bi + bankOffsetB];
+}
+
+
+
+// **===-------- Lab4: Modify the body of this function -----------===**
+// You may need to make multiple kernel calls, make your own kernel
+// function in this file, and then call them from here.
+void prescanArray(float *outArray, float *inArray, int numElements) {
+  
+  dim3 dimGrid(6, 6, 1);
+	dim3 dimBlock(numElements, 4, 1);
+
+	// Launch the device computation threads!
+  //naiveScan<<<dimGrid, dimBlock, 2*numElements>>>(outArray, inArray, numElements);
+  //sweepScan<<<dimGrid, dimBlock, 2*numElements>>>(outArray, inArray, numElements);
+  //simp<<<dimGrid, dimBlock, 2*numElements>>>(outArray, inArray, numElements);
+}
+// **===-----------------------------------------------------------===**
+
+
+
+
+
+
+
+__global__ void sweepScan(float *g_odata, float *g_idata, int n) {
+  extern __shared__ float temp[]; // allocated on invocation
+  
+  int tid = threadIdx.x;
+  int offset = 1;
+  
+  temp[2*tid] = g_idata[2*tid]; //load 1st and second element into shared mem
+  temp[2*tid+1] = g_idata[2*tid+1];
+  
+  for (int d = n >> 1; d > 0; d >>= 1) {  // build sum in place up the tree
+    //divide n by 2 each time, probably shift so no decimals
+    __syncthreads();
+    
+    if (tid < d) {
+      int ai = offset * (2 * tid + 1) - 1;
+      int bi = offset * (2 * tid + 2) - 1;
+      
+      temp[bi] += temp[ai];
+    }
+    
+    offset *= 2;
+  }
+  
+  if (tid == 0) {
+    temp[n-1] = 0; // clear the last element
+  }
+  
+  for (int d = 1; d < n; d *= 2)  {//traverse down tree & build scan
+    offset >>= 1;
+    __syncthreads();
+    
+    if (tid < d) {
+      int ai = offset*(2*tid+1)-1; 
+      int bi = offset*(2*tid+2)-1; 
+      
+      float t = temp[ai];
+      temp[ai] = temp[bi]; 
+      temp[bi] += t;
+    }
+  }
+  
+  __syncthreads();
+  
+  g_odata[2*tid] = temp[2*tid];  // write results to device memory
+  g_odata[2*tid+1] = temp[2*tid+1];
+}
+
+
+
+__global__ void naiveScan(float *g_odata, float *g_idata, int n) {
 
   extern __shared__ float temp[]; // allocated on invocation
   
@@ -50,39 +189,5 @@ __global__ void scanKernel(float *g_odata, float *g_idata, int n) {
   g_odata[tid] = temp[buffer1*n+tid]; // write output
   
 }
-
-
-__global__ void simp(float* output, float* input) {
-  int tid = threadIdx.x;
-  
-  unsigned int i = 2*tid;
-  
-  __syncthreads();
-  
-  for (unsigned int stride = 1; stride <= blockDim.x; stride *= 2) {
-    if (tid % stride == 0) {
-      input[i] += input[i + stride];
-    }
-    __syncthreads();
-  }
-  if(tid == 0) {
-    *output = input[0];
-  }
-}
-
-// **===-------- Lab4: Modify the body of this function -----------===**
-// You may need to make multiple kernel calls, make your own kernel
-// function in this file, and then call them from here.
-void prescanArray(float *outArray, float *inArray, int numElements) {
-  
-  dim3 dimGrid(6, 6, 1);
-	dim3 dimBlock(numElements, 4, 1);
-
-	// Launch the device computation threads!
-  scanKernel<<<dimGrid, dimBlock, numElements*2>>>(outArray, inArray, numElements);
-  //simp<<<dimGrid, dimBlock>>>(outArray, inArray);
-}
-// **===-----------------------------------------------------------===**
-
 
 #endif // _PRESCAN_CU_
